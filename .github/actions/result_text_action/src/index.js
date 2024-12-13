e0509070d7b7ee6f213749cd267a8e86fc38efba
KikeValeroSoriano <kikevalerosoriano@gmail.com>
2024-12-13 15:17:00 +0100

    User -> Kike Valero - Afegint linter-job, cypress-job i add-badge-job

---
+++ .github/actions/result_text_action/src/index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const fs = require('fs').promises;
+
+async function main() {
+    try {        
+        console.log('Starting the script...');
+        console.log('Mostrem el directori actual de treball:', process.cwd());
+
+        //Obtinguem el resultat del test.
+        const resultat_test = core.getInput('test_result');
+        //Guardem l´insignia de error en cas de que el test falle.
+        const img_error = 'https://img.shields.io/badge/test-failure-red';
+        //Guardem l´insignia del test en cas de que el test resulte exitós.
+        const img_exit = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg';
+        //Guardem l´insignia que mostrarem al readme depenent del resultat del test.
+        const badge = resultat_test === 'success' ? img_exit : img_error;
+        //Guardem el missatge que afegirem abans del l´insignia al readme.
+        const missatge_badge = `RESULTAT DELS ÚLTIMS TESTS \n ![Test result badge](${badge})`;
+
+        const oldReadmePath = './OldREADME.md';
+        let oldReadmeContent = await fs.readFile(oldReadmePath, 'utf-8');
+        let newReadmeContent = oldReadmeContent + "\n" + missatge_badge;
+        const readmePath = './README.md';
+        await fs.writeFile(readmePath, newReadmeContent);
+
+        process.exit(0);
+    } catch (e) {
+        console.error(e);
+        core.setFailed(e.message);
+    }
+
+};
+
+main();
