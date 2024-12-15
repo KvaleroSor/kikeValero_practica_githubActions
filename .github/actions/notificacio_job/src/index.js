c9f98a97a42371a5693324252d88e5091e6b5891
KikeValeroSoriano <kikevalerosoriano@gmail.com>
2024-12-15 09:16:24 +0100

    User -> Kike Valero - Modificant script per notification_job

--- .github/actions/notificacio_job/src/index.js
+++ .github/actions/notificacio_job/src/index.js
@@ -1,46 +1,58 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
+const {google} = require("googleapis");
 const servicioEmail = require("nodemailer");
 require("dotenv").config();
 
 async function run() {
     try {
-        const destinatari_email = core.getInput("destinatari_email"); 
+        const destinatari_email = core.getInput("destinatari_email");
         const assumpte_email = "Resultat del Workflow executat";
-        const cos_email = "S'ha realitzat un push en la branca main que ha provocat l'execució del workflow nom_repositori_workflow amb els següents resultats:\n";        
+        const cos_email =
+            "S'ha realitzat un push en la branca main que ha provocat l'execució del workflow nom_repositori_workflow amb els següents resultats:\n";
         const linter_job = core.getInput("linter_job");
         const cypress_job = core.getInput("cypress_job");
         const badge_job = core.getInput("badge_job");
         const deploy_job = core.getInput("deploy_job");
         const remitent_email = core.getInput("remitent_email");
-        const remitent_password = core.getInput("remitent_password");
+        // const remitent_password = core.getInput("remitent_password");
         const client_id = core.getInput("client_id");
         const client_secret = core.getInput("client_secret");
         const refresh_token = core.getInput("refresh_token");
-        const acces_token = core.getInput("acces_token");
+        // const acces_token = core.getInput("acces_token");
+
+        const OAuth2 = google.auth.OAuth2;
+        const oauth2Client = new OAuth2(
+            client_id,
+            client_secret,
+            "https://developers.google.com/oauthplayground"
+        );
+        
+        oauth2Client.setCredentials({ refresh_token: refresh_token }); // Obtenemos el accessToken usando el refreshToken const accessToken = await oauth2Client.getAccessToken();
+        const accessToken = await oauth2Client.getAccessToken();
 
         const transporter = servicioEmail.createTransport({
-            service: "gmail",            
+            service: "gmail",
             auth: {
                 type: "OAuth2",
                 user: remitent_email,
                 clientId: client_id,
-                clientSecret: client_secret,    
+                clientSecret: client_secret,
                 refreshToken: refresh_token,
-                accessToken: acces_token
+                accessToken: accessToken.token,
             },
         });
 
         const email_options = {
-            from: 'remitent_email',
+            from: remitent_email,
             to: destinatari_email,
             subject: assumpte_email,
             text: cos_email + `\n
             - Linter job --> ${linter_job}\n,
             - Cypress job --> ${cypress_job}\n,
             - Badge job --> ${badge_job}\n,
-            - Deploy job --> ${deploy_job}\n`            
-        }
+            - Deploy job --> ${deploy_job}\n`,
+        };
 
         transporter.sendMail(email_options, (error, info) => {
             if (error) {
